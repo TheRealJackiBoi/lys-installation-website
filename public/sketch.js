57e98459e4c13f95053e27ddf166dc82350c08f6
TheRealJackiBoi <jslam@oulund.dk>
2021-02-25 22:39:56 +0100

    NEW AND DONE

---
+++ public/sketch.js
@@ -0,0 +1,58 @@
+var socket;
+
+function setup() {
+  socket = io.connect('http://localhost:8000');
+  /*socket.on('mouse',
+    function(data) {
+      console.log("Got: " + data.x + " " + data.y);
+      fill(0,0,255);
+      noStroke();
+      ellipse(data.x, data.y, 20, 20);
+    }
+  );*/
+
+
+  let left = createButton("Left"); 
+  left.mousePressed(()=>{sendData("left");});
+  left.id('left');
+  left.class('game-button');
+  left.position(screen.width/2 - 180, 240);
+
+  let right = createButton("Right"); 
+  right.mousePressed(()=>{sendData("right");});
+  right.id('right');
+  right.class('game-button');
+  right.position(screen.width/2 + 70, 240);
+
+  let up = createButton("Up"); 
+  up.mousePressed(()=>{sendData("up");});
+  up.id('up');
+  up.class('game-button');
+  up.position(screen.width/2 - 60, 120);
+  
+  let down = createButton("Down"); 
+  down.mousePressed(()=>{sendData("down");});
+  down.id('down');
+  down.class('game-button');
+  down.position(screen.width/2 - 60, 380);
+}
+
+function draw() {
+  // Nothing
+}
+
+
+// Function for sending to the socket
+function sendData(d) {
+  
+  // Make a little object with  and y
+  var data = {
+    d: d,
+  };
+
+  // Send that object to the socket
+  socket.emit('control',data);
+}
+
+
+console.log('running...')
---
+++ server.js
@@ -0,0 +1,50 @@
+var path = require('path');
+console.log("My socket server is running");
+
+var express = require('express');
+var app = express();
+
+var server = app.listen(process.env.PORT || 8000, listen);
+
+function listen() {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('Example app listening at http://' + host + ':' + port);
+}
+
+app.use(express.static('public'));
+
+let direction = "left";
+
+app.get('/', function(request, response) {
+  response.sendFile(__dirname + '/views/index.html');
+});
+
+app.get('/control', function(request, response) {
+  response.send(direction);
+});
+
+
+
+var io = require('socket.io')(server);
+
+io.sockets.on('connection',
+  function (socket) {
+  
+    console.log("We have a new client: " + socket.id);
+  
+    socket.on('control',
+      function(data) {
+        console.log("Received new direction: " + data.d);
+
+        direction = data.d;
+
+      }
+    );
+    
+    socket.on('disconnect', function() {
+      console.log("Client has disconnected");
+    });
+  }
+);
+
